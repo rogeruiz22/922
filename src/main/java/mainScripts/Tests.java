f3c3737122b7e7e83ac90378c0a0a67d9958f5e7
Roger Ruiz <roger.ruiz@oktana.com>
2022-05-12 13:43:48 -0400

    Novena vez

--- src/main/java/mainScripts/Tests.java
+++ src/main/java/mainScripts/Tests.java
@@ -4,9 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class Main {
+public class Tests {
 
+	@Test
 	public static void main(String[] args) throws Exception {
 		WebDriver driver = new ChromeDriver();
 		
