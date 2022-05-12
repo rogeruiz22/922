382d2891acdb74fdab3eebaa6d18385cfa6fa442
Roger Ruiz <roger.ruiz@oktana.com>
2022-05-12 13:21:03 -0400

    octava vez

--- src/main/java/mainScripts/Main.java
+++ src/main/java/mainScripts/Main.java
@@ -1,6 +1,8 @@
 package mainScripts;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
@@ -8,9 +10,18 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://login.salesforce.com");
+		driver.get("https://teamorg2-dev-ed.my.salesforce.com/");
 		
-		Thread.sleep(5000);
+		WebElement usernameBox = driver.findElement(By.xpath("//input[@id='username']"));
+		WebElement passwordBox = driver.findElement(By.xpath("//input[@id='password']"));
+		WebElement loginButton = driver.findElement(By.xpath("//input[@id='Login']"));
+		
+		usernameBox.sendKeys("incubatorteam@oktana.com");
+		passwordBox.sendKeys("incubator2022");
+		
+		loginButton.click();
+		
+		Thread.sleep(7000);
 		
 		driver.quit();
 	}
