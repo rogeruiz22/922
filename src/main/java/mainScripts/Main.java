54f6d288003a6378b389966a2be65830d42e58cd
Roger Ruiz <roger.ruiz@oktana.com>
2022-05-12 09:49:02 -0400

    File configuration uploaded with new modifications

--- src/main/java/mainScripts/Main.java
+++ src/main/java/mainScripts/Main.java
@@ -1,10 +1,18 @@
 package mainScripts;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://login.salesforce.com");
+		
+		Thread.sleep(5000);
+		
+		driver.quit();
 	}
 
 }
