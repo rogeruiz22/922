748f098c5d3a6aec46f71a44bf183f15964fda2f
Roger Ruiz <roger.ruiz@oktana.com>
2022-05-13 16:21:42 -0400

    Ahora si funciona, por fiiiin wuiii

--- src/main/java/pruebas/Main.java
+++ src/main/java/pruebas/Main.java
@@ -8,22 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://teamorg2-dev-ed.my.salesforce.com/");
-		
-		WebElement usernameBox = driver.findElement(By.xpath("//input[@id='username']"));
-		WebElement passwordBox = driver.findElement(By.xpath("//input[@id='password']"));
-		WebElement loginButton = driver.findElement(By.xpath("//input[@id='Login']"));
-		
-		usernameBox.sendKeys("incubatorteam@oktana.com");
-		passwordBox.sendKeys("incubator2022");
-		
-		loginButton.click();
-		
-		Thread.sleep(7000);
-		
-		driver.quit();
 	}
 
 }
